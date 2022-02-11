97be44b7f3913001be256586cecbd39a1ce43afc
Mizter Egoist <mizteregoist@gmail.com>
2022-02-11 12:13:00 +0300

    Basic version. Updates later.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # localserver
 The simple docker container helps you to develop more awesome web apps
+
+If you haven't python3 on your machine, install it for generating .env file.
+
+Before run docker container you need to generate .env file by run command:
+> python init_env.py
+
+After generating .env file you can do these command to run docker container:
+> docker-compose up -d --build
+
+If you run localserver on Windows you can get errors about opening ports, if ports aren't opened you can run these command in terminal of administrator permissions:
+> net stop hns
+> 
+> net start hns
+
+PostgreSQL and MySQL store data in ./data/db directory.
+Be sure you have these directories in localserver directory.
+
+Logs stores at ./logs
+
+For connect to PostgreSQL and MySQL use host:
+> host.docker.internal
---
+++ init_env.py
@@ -0,0 +1,44 @@
+import shutil
+import string
+import random
+from pathlib import Path
+
+
+def str_generator(size=16):
+    chars = string.ascii_uppercase + string.digits + string.ascii_lowercase
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def init():
+    try:
+        current_dir = Path(__file__).resolve().parent
+        env_path = current_dir.joinpath('.env')
+        pgsql_pass = str_generator()
+        pgadmin_pass = str_generator()
+        mysql_pass = str_generator()
+        mysql_root_pass = str_generator()
+        data = f"""# Init .env
+PGSQL_USER=webapp
+PGSQL_PASS={pgsql_pass}
+PGSQL_NAME=webapp
+
+PGADMIN_EMAIL=admin@webapp.local
+PGADMIN_PASS={pgadmin_pass}
+
+MYSQL_USER=webapp
+MYSQL_PASS={mysql_pass}
+MYSQL_NAME=webapp
+MYSQL_ROOT_PASS={mysql_root_pass}
+
+PMA_USER=root
+PMA_PASSWORD={mysql_root_pass}
+"""
+        env = open(env_path, 'x')
+        env.write(data)
+        env.close()
+    except FileExistsError as e:
+        print('.env already exists, delete it if you want to create a new one')
+
+
+if __name__ == '__main__':
+    init()
